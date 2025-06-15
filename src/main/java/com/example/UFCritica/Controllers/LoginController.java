2eab42b78eb4ed4e35882320671a8abc3c05ecb6
Robert-099 <robertoliveira996@gmail.com>
2025-06-14 20:14:54 -0400

    atualizacao pagina de inicial e cadastro

--- src/main/java/com/example/UFCritica/Controllers/LoginController.java
+++ src/main/java/com/example/UFCritica/Controllers/LoginController.java
@@ -1,5 +1,13 @@
 package com.example.UFCritica.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class LoginController {
+    @GetMapping("/register") // Este método será executado quando alguém acessar /register
+    public String showRegistrationForm() {
+        return "register"; // Retorna o nome da sua view (o arquivo register.html)
+    }
+
+
     
 }
